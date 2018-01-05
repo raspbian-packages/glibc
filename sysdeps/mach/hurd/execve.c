c85fb49458fe1f2d6f7b665038aabcf381070fe7
GNU Libc Maintainers <debian-glibc@lists.debian.org>
2018-01-05 18:09:39 +0000

    submitted-exec_filename
    2.13-33 dates when this was added
    
    TODO: _DEBIAN_ in versions however pose problem. Remove the _DEBIAN_ version
    once packages are rebuilt against 2.21.
    
    2010-08-04  Emilio Pozuelo Monfort  <pochu27@gmail.com>
    2017-09-26  Svante Signell  <svante.signell@gmail.com>
    	* hurd/hurdexec.c (_hurd_exec): Deprecate it.
    	(_hurd_exec_file_name): New function.
    	* hurd/hurd.h (_hurd_exec): Deprecate it.
    	(_hurd_exec_file_name): Declare it.
    	* hurd/Versions: Export it.
    	* sysdeps/mach/hurd/execve.c: Use it.
    	* sysdeps/mach/hurd/fexecve.c: Likewise.
    	* sysdeps/mach/hurd/spawni.c: Likewise.
    
    
    From d1793416cf8bf6fccd42679a8ec30b0058823ab8 Mon Sep 17 00:00:00 2001
    From: Emilio Pozuelo Monfort <pochu27@gmail.com>
    Date: Sat, 22 May 2010 18:26:29 +0200
    Subject: [PATCH] Use the new file_exec_file_name RPC
    
    Pass the file name of executable to the exec server, which it needs to
    execute #!-scripts.  Currently, the exec server tries to guess the name
    from argv[0] but argv[0] only contains the executable name by convention.
    
    Gbp-Pq: Topic hurd-i386
    Gbp-Pq: Name submitted-exec_filename.diff

--- sysdeps/mach/hurd/execve.c
+++ sysdeps/mach/hurd/execve.c
@@ -18,6 +18,8 @@
 #include <unistd.h>
 #include <hurd.h>
 #include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /* Replace the current process, executing FILE_NAME with arguments ARGV and
    environment ENVP.  ARGV and ENVP are terminated by NULL pointers.  */
@@ -25,17 +27,45 @@ int
 __execve (const char *file_name, char *const argv[], char *const envp[])
 {
   error_t err;
-  file_t file = __file_name_lookup (file_name, O_EXEC, 0);
+  char *concat_name = NULL;
+  const char *abs_name;
 
+  file_t file = __file_name_lookup (file_name, O_EXEC, 0);
   if (file == MACH_PORT_NULL)
     return -1;
 
+  if (file_name[0] == '/')
+    {
+      /* Absolute path */
+      abs_name = file_name;
+    }
+  else
+    {
+      /* Relative path */
+      char *cwd = __getcwd (NULL, 0);
+      if (cwd == NULL)
+	{
+	  __mach_port_deallocate (__mach_task_self (), file);
+	  return -1;
+	}
+      int res = __asprintf (&concat_name, "%s/%s", cwd, file_name);
+      free (cwd);
+      if (res == -1)
+	{
+	  __mach_port_deallocate (__mach_task_self (), file);
+	  return -1;
+	}
+      abs_name = concat_name;
+    }
+
   /* Hopefully this will not return.  */
-  err = _hurd_exec (__mach_task_self (), file, argv, envp);
+  err = _hurd_exec_file_name (__mach_task_self (), file,
+			      abs_name, argv, envp);
 
   /* Oh well.  Might as well be tidy.  */
   __mach_port_deallocate (__mach_task_self (), file);
 
+  free (concat_name);
   return __hurd_fail (err);
 }
 
