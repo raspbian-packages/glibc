113531be97e112704bfd46b80a9d1a8927fae404
GNU Libc Maintainers <debian-glibc@lists.debian.org>
2024-04-23 19:23:00 +0300

    libpthread_version
    This dates when libpthread was moved to glibc and some features were added.
    
    2.13-31/38/39 dates when the global signal disposition call was added.
    TODO: _DEBIAN_ in versions however pose problem. Remove the _DEBIAN_ version
    once packages are rebuilt against 2.21.
    
    
    Gbp-Pq: Topic hurd-i386
    Gbp-Pq: Name libpthread_version.diff

--- sysdeps/mach/hurd/htl/pt-hurd-cond-wait.c
+++ sysdeps/mach/hurd/htl/pt-hurd-cond-wait.c
@@ -22,6 +22,8 @@
 
 #include <pt-internal.h>
 
+#include <shlib-compat.h>
+
 /* Implemented in pt-hurd-cond-timedwait.c.  */
 extern int __pthread_hurd_cond_timedwait_internal (pthread_cond_t *cond,
 						   pthread_mutex_t *mutex,
@@ -36,5 +38,17 @@ __pthread_hurd_cond_wait_np (pthread_cond_t *cond, pthread_mutex_t *mutex)
   err = __pthread_hurd_cond_timedwait_internal (cond, mutex, NULL);
   return err == EINTR;
 }
+versioned_symbol (libpthread, __pthread_hurd_cond_wait_np, pthread_hurd_cond_wait_np, GLIBC_2_21);
 
-strong_alias (__pthread_hurd_cond_wait_np, pthread_hurd_cond_wait_np);
+#if SHLIB_COMPAT (libpthread, GLIBC_2_13, GLIBC_2_21)
+int
+__pthread_hurd_cond_wait_np_2_13 (pthread_cond_t *cond,
+			     pthread_mutex_t *mutex)
+{
+  error_t err;
+
+  err = __pthread_hurd_cond_timedwait_internal (cond, mutex, NULL);
+  return (err == EINTR);
+}
+compat_symbol (libpthread, __pthread_hurd_cond_wait_np_2_13, pthread_hurd_cond_wait_np, GLIBC_2_13_DEBIAN_38);
+#endif
