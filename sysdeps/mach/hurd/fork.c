8106d571e4bf4fcbb6feb716ff89314a89934854
Jeremie Koenig <jk@jk.fr.eu.org>
2024-06-29 13:27:34 +0300

    [PATCH] Small signal fixes
        22e7268 Hurd signals: fix sigwait for pending signals
        da8bf5e Hurd signals: clear the pending mask in fork and spawn
        8e87205 Hurd signals: don't drop blocked ignored signals     
    
    
    Gbp-Pq: Topic hurd-i386
    Gbp-Pq: Name tg-hurdsig-fixes-2.diff

--- sysdeps/mach/hurd/fork.c
+++ sysdeps/mach/hurd/fork.c
@@ -655,8 +655,10 @@ __fork (void)
       err = __USEPORT (PROC, __proc_getpids (port, &_hurd_pid, &_hurd_ppid,
 					     &_hurd_orphaned));
 
-      /* Forking clears the trace flag.  */
+      /* Forking clears the trace flag and pending masks.  */
       __sigemptyset (&_hurdsig_traced);
+      __sigemptyset (&_hurd_global_sigstate->pending);
+      __sigemptyset (&ss->pending);
 
       /* Release malloc locks.  */
       _hurd_malloc_fork_child ();
