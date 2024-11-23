d743d78c6ee5bae60fb10247a54880bfb6081dd7
Samuel Thibault <samuel.thibault@ens-lyon.org>
2024-11-23 14:57:03 +0100

    [PATCH] The critical section lock _can_ be held in these place.
    At least since hurd_thread_cancel can be called by another thread and lock our
    critical lock.
    
    http://bugs.debian.org/46859
    
    “
    Thomas suggested that there is no need to take the critical section
    lock.  I believe that taking the critical section lock is necessary to
    prevent the target thread from entering a signal handler.  Roland will
    look into the problem.
    ”
    
    Taking the critical section lock makes these assertions bogus.
    
    It happens that hurd_thread_cancel is only called from libports and inside
    /hurd/term so this is rare in practice.
    
    A reproducer can be found here:
    
    http://lists.gnu.org/archive/html/bug-hurd/2014-05/msg00025.html
    
    2006-08-05  Samuel Thibault  <samuel.thibault@ens-lyon.org>
    
           * hurd/thread-cancel.c (hurd_thread_cancel): Do not assert that
           `&ss->critical_section_lock' is unlocked.
           * sysdeps/mach/hurd/jmp-unwind.c (_longjmp_unwind): Likewise, and take
           critical section lock before taking the sigstate lock.
           * sysdeps/mach/hurd/spawni.c (__spawni): Likewise.
    
    
    Gbp-Pq: Topic hurd-i386
    Gbp-Pq: Name tg-thread-cancel.diff

--- hurd/thread-cancel.c
+++ hurd/thread-cancel.c
@@ -42,7 +42,6 @@ hurd_thread_cancel (thread_t thread)
       return 0;
     }
 
-  assert (! __spin_lock_locked (&ss->critical_section_lock));
   __spin_lock (&ss->critical_section_lock);
   __spin_lock (&ss->lock);
   err = __thread_suspend (thread);
@@ -82,7 +81,6 @@ hurd_check_cancel (void)
   int cancel;
 
   __spin_lock (&ss->lock);
-  assert (! __spin_lock_locked (&ss->critical_section_lock));
   cancel = ss->cancel;
   ss->cancel = 0;
   __spin_unlock (&ss->lock);
