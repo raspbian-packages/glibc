a251cb397d2c5fe7a38c0f89a4b327defb13fc54
GNU Libc Maintainers <debian-glibc@lists.debian.org>
2026-01-19 20:12:24 +0100

    git-updates
    GIT update of https://sourceware.org/git/glibc.git/release/2.42/master from glibc-2.42 to 453e6b8dbab935257eb0802b0c97bca6b67ba30e
    
    GIT update of https://sourceware.org/git/glibc.git/release/2.42/master from glibc-2.42 to 453e6b8dbab935257eb0802b0c97bca6b67ba30e
    
    
    Gbp-Pq: Name git-updates.diff

--- sysdeps/powerpc/powerpc64/multiarch/ifunc-impl-list.c
+++ sysdeps/powerpc/powerpc64/multiarch/ifunc-impl-list.c
@@ -164,6 +164,9 @@ __libc_ifunc_impl_list (const char *name, struct libc_ifunc_impl *array,
   /* Support sysdeps/powerpc/powerpc64/multiarch/strncmp.c.  */
   IFUNC_IMPL (i, name, strncmp,
 #ifdef __LITTLE_ENDIAN__
+	      IFUNC_IMPL_ADD (array, i, strncmp, hwcap2 & PPC_FEATURE2_ARCH_3_1
+			      && hwcap & PPC_FEATURE_HAS_VSX,
+			      __strncmp_power10)
 	      IFUNC_IMPL_ADD (array, i, strncmp, hwcap2 & PPC_FEATURE2_ARCH_3_00
 			      && hwcap & PPC_FEATURE_HAS_ALTIVEC,
 			      __strncmp_power9)
@@ -366,6 +369,10 @@ __libc_ifunc_impl_list (const char *name, struct libc_ifunc_impl *array,
   /* Support sysdeps/powerpc/powerpc64/multiarch/strcmp.c.  */
   IFUNC_IMPL (i, name, strcmp,
 #ifdef __LITTLE_ENDIAN__
+	      IFUNC_IMPL_ADD (array, i, strcmp,
+			      (hwcap2 & PPC_FEATURE2_ARCH_3_1)
+			      && (hwcap & PPC_FEATURE_HAS_VSX),
+			      __strcmp_power10)
 	      IFUNC_IMPL_ADD (array, i, strcmp,
 			      hwcap2 & PPC_FEATURE2_ARCH_3_00
 			      && hwcap & PPC_FEATURE_HAS_ALTIVEC,
